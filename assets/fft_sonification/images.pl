ea575c930d5d5315a41e97df5cd6d51bcd409d59
rcompton <compton.ryan@gmail.com>
2014-05-24 00:45:00 -0700

    FFT sonification

---
+++ assets/fft_sonification/images.pl
@@ -0,0 +1,92 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/X_{text{even}k};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="59" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ X_{\text{even }k}$">|; 
+
+$key = q/{x_n}_{n=0}^{N-1};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="74" HEIGHT="42" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ \{ x_n \}_{n=0}^{N-1}$">|; 
+
+$key = q/N=1;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="56" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$ N=1$">|; 
+
+$key = q/displaystylesum_{n=0}^{Nslash2-1}x_{2n}e^{frac{-2pii}{N}(2n)k}+sum_{n=0}^{Nslash2-1}x_{2n+1}e^{frac{-2pii}{N}(2n+1)k};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="351" HEIGHT="82" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$\displaystyle \sum_{n=0}^{N/2-1} x_{2n} e^{\frac{-2 \pi i}{N}(2n)k} + \sum_{n=0}^{N/2-1} x_{2n+1} e^{\frac{-2 \pi i}{N}(2n+1)k}$">|; 
+
+$key = q/X_{text{odd}k};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="55" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$ X_{\text{odd }k}$">|; 
+
+$key = q/k;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ k$">|; 
+
+$key = q/displaystyleX_k=sum_{n=0}^{N-1}x_ne^{frac{-2pii}{N}nk};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="161" HEIGHT="79" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\displaystyle X_k = \sum_{n=0}^{N-1} x_n e^{\frac{-2 \pi i}{N}nk}
+$">|; 
+
+$key = q/displaystyleX_k;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="29" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$\displaystyle X_k$">|; 
+
+$key = q/displaystyleX_{text{even}k}+e^{frac{-2pii}{N}k}X_{text{odd}k};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="180" HEIGHT="48" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$\displaystyle X_{\text{even }k} + e^{\frac{-2 \pi i}{N}k} X_{\text{odd }k}$">|; 
+
+$key = q/0,...,N-1;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="96" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ 0, ..., N-1$">|; 
+
+$key = q/displaystylex_n=220.0*2^{frac{n}{12}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="136" HEIGHT="43" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$\displaystyle x_n = 220.0*2^{\frac{n}{12}}
+$">|; 
+
+$key = q/{X_k}_{k=0}^{N-1};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="78" HEIGHT="42" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ \{ X_k \}_{k=0}^{N-1}$">|; 
+
+$key = q/{x_n};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="44" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$ \{ x_n \}$">|; 
+
+$key = q/displaystyle=;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\displaystyle =$">|; 
+
+1;
+
