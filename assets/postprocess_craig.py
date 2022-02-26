577c13a369934f969eba08bf9013c5ffa1baf155
rcompton <ryan@ryancompton.net>
2022-02-26 18:43:49 +0000

    logger

--- assets/postprocess_craig.py
+++ assets/postprocess_craig.py
@@ -44,7 +44,6 @@ def s3_to_dics(s3_fname):
                   'crawl_date', 'geo.placename', 'mapaddress', 'og:title'
                  ])
     dics = []
-    logger.info(f"working on: {s3_fname}")
     for line in smart_open.smart_open(s3_fname):
         dic = json.loads(line)
         furnished_hack = False
@@ -62,14 +61,17 @@ def s3_to_dics(s3_fname):
         if 'price' in dic and (pd.isna(dic['post_price'])):
             dic['post_price'] = float(price_dec(dic['price'])) # should not use floats for money but whatever
         dics.append(dic)
+        if 'data_accuracy' not in dic:
+            dic['data_accuracy'] = 9999
     try:
-        logger.info(f'writing parsed to cragprod: {s3_fname}')
         df = pd.DataFrame(dics)
         df = postprocess_df(df)
         df.to_sql('cragprod', engine, if_exists='append', index=False)
-        logger.info(f'done! writing: {s3_fname}')
+        logger.info(f'wrote to cragprod: {s3_fname}')
+    except KeyError:
+        logger.info(f'KeyError, skipping. df.shape: {df.shape}')
     except:
-        logger.exception('database meh')
+        logger.exception('Real database issue')
     return
 
 def parse_dir(date, s3dir = 's3://rycpt-crawls/craigslist-housing/'):
@@ -95,15 +97,15 @@ def parse_dir(date, s3dir = 's3://rycpt-crawls/craigslist-housing/'):
 def postprocess_df(df):
     #df=df[df['mapaddress'].notnull()]
     #df=df[df['data_accuracy'].notnull()]
-    logger.warning(f'df.shape init {df.shape}')
+    #logger.warning(f'df.shape init {df.shape}')
     df = df[df.price.notnull()]
-    logger.warning(f'df.shape with price {df.shape}')
+    #logger.warning(f'df.shape with price {df.shape}')
     df = df[df.post_price.notnull()]
-    logger.warning(f'df.shape with post_price {df.shape}')
+    #logger.warning(f'df.shape with post_price {df.shape}')
     df['data_accuracy'] = df['data_accuracy'].map(lambda x: int(x) if pd.notnull(x) else None)
-    logger.warning(f'df.shape with any data_accuracy {df.shape}')
+    #logger.warning(f'df.shape with any data_accuracy {df.shape}')
     df = df[df.data_accuracy<11]
-    logger.warning(f'df.shape with any data_accuracy < 11 {df.shape}')
+    #logger.warning(f'df.shape with any data_accuracy < 11 {df.shape}')
     df['netloc'] = df['og:url'].map(lambda x: urllib.parse.urlparse(x).netloc)
     df['price_per_sqft'] = df['post_price']/df['post_sqft']
     #df = df[df.post_price < 10000]
